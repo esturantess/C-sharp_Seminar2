6a026fc16f23b7def68dc6f4477d28bf7251c7d2
Polina <ms.maylin@inbox.ru>
2023-01-20 17:29:49 +0300

    Example1 added

--- Example1/Program.cs
+++ Example1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random rand = new Random(); // Объявление объекта класса Random
+int randNumber = rand.Next(10, 100); // Генерация переменной от 10 до 99 -- [10; 100) || [10; 99]
+// int randNumber2 = new Random().Next(10, 100);
+System.Console.WriteLine(randNumber);
+
+if (randNumber / 10 > randNumber % 10)
+{
+System.Console.WriteLine($"Большая цифра числа {randNumber}: {randNumber / 10}");
+}
+else
+{
+System.Console.WriteLine($"Большая цифра числа {randNumber}: {randNumber % 10}");
+}
